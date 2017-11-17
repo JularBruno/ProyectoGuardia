c4dfa6efcac1d99b91821de2e745addbb80f6781
Laseitor3000 <santi.lasa99@gmail.com>
2017-11-17 09:26:16 -0300

    se modificaron los html y se agregaron funciones de java script

--- config/urls.py
+++ config/urls.py
@@ -1,29 +1,7 @@
-"""django101 URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
-from guardia.views import *
 
 urlpatterns = [
+    url(r'^', include('guardia.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^index/$', index, name="index"),
-    url(r'^upload$', upload, name="upload"),
-    url(r'^registro/$', registro, name="registros"),
-    url(r'^salida/$', salida, name="salidas"),
-   # url(r'^familia/$', famila, name="familias"),
-
-
 ]
--- guardia/views.py
+++ guardia/views.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, redirect
+
 
 from .models import *
+
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.shortcuts import render_to_response, render, redirect
+from django.template import RequestContext
+from django.conf import settings
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import AnonymousUser
+from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Q
+from datetime import datetime, timedelta
+from django.utils import timezone
 
 # Create your views here.
 
@@ -21,8 +35,8 @@ def salida(request):
     return render(request, "salidas.html", {"guardias": tercero})
 
 #def familia(request):
- #   cuarto = guardia.objects.all()
-  #  return render(request, "familias.html", {"guardias": cuarto})
+#   cuarto = guardia.objects.all()
+#  return render(request, "familias.html", {"guardias": cuarto})
 
 
 def upload(request):
@@ -31,3 +45,20 @@ def upload(request):
     new.save()
     #return render(request, "base.html", {"guardias": [new.nombre]})
     return redirect(index)
+
+
+
+def loginv(request):
+    if request.method != "POST":
+        return redirect('index')
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user != None:
+        login(request, user)
+        return redirect('index')
+
+def logoutv(request):
+    logout(request)
+    cuarto = guardia.objects.all()
+    return render(request, "log-in.html", {"guardias": cuarto})
